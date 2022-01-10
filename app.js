9ebcd413b2e4a519f5d7864cf2560eaa9887718e
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-10 23:54:38 +0530

    component - final

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: 'rajesh',
@@ -18,11 +17,37 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'rajesh',
+        name: 'Rajesh Kumar',
+        phone: '01234 5678 991',
+        email: 'rajesh@localhost.com',
+      }
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
-    }
-  }
+    },
+  },
 });
 
 app.mount('#app');
