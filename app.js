cda8a6abe0f98bcbf2351065946455be42c6b1f6
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-10 21:32:49 +0530

    vue app logic

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log();
+const app = Vue.createApp({
+  data() {
+    return {
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: 'rajesh',
+          name: 'Rajesh Kumar',
+          phone: '01234 5678 991',
+          email: 'rajesh@localhost.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '09876 543 221',
+          email: 'julie@localhost.com',
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+});
+
+app.mount('#app');
